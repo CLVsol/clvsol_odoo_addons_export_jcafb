786ed25bd0dd5714689c9cdd37be9053e9cab45f
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2021-03-12 14:09:55 -0300

    [IMP] clv_lab_test_export_jcafb (IMP __manifest__.py)

--- clv_lab_test_export_jcafb/__manifest__.py
+++ clv_lab_test_export_jcafb/__manifest__.py
@@ -5,7 +5,7 @@
 {
     'name': 'Lab Test Export (for CLVhealth-JCAFB Solution)',
     'summary': 'Lab Test Export Module used in CLVhealth-JCAFB Solution.',
-    'version': '12.0.4.0',
+    'version': '14.0.5.0',
     'author': 'Carlos Eduardo Vercelino - CLVsol',
     'category': 'CLVsol Solutions',
     'license': 'AGPL-3',
